eb102a9589dfd8596eeaebcc0d9f112b57cbaf88
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-30 14:30:46 +0300

    ading button to heerosection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Box, Stack, Typography} from '@mui/material';
+import { Box, Button, Stack, Typography} from '@mui/material';
+
+
 
 const HeroSection = () => {
   return (
-    <Box>
-      <Typography>
+    <Box sx={{
+      mt:{ lg: '212px', xs: '70px' }, ml : {sm : '50px'}
+    }} positon='relative' p='20px'>
+      <Typography color={'navy'} fontWeight= '500' fontSize={'30px'}>
         replace
       </Typography>
+      <Typography fontWeight={700}
+       sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px"
+      >
+        No pain, <br/> no gain
+      </Typography>
+      <Button href='#exercises' variant='contained' color='success'> More Exercises</Button>
     </Box>
   )
 }
