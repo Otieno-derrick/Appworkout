597675dd6ba0d3addd26b87dd3726787e9b33cb1
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-30 12:40:06 +0300

    creating routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import {Box} from '@mui/material';
+import Home from "./pages/Home";
+import ExerciseDetails from './pages/ExersiceDetails';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
-        <Box width="400px">
+        <Box width="400px" sx={{ width: {xl:' 1448px'}}} m="auto">
             <Routes>
                <Route path='/' element={<Home />} />
-               <Route path='/' element={<ExerciseDetail />} />
+               <Route path='/' element={<ExerciseDetails />} />
             </Routes>
+            <Footer />
         </Box>
 
     </div>
